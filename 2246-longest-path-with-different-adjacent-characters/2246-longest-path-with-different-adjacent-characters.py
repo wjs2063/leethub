eb7101dace23fd919fd0aa44593e6cad52962840
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-01-13 21:10:20 +0900

    Time: 1612 ms (94.54%), Space: 149 MB (76.92%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -1,20 +1,21 @@
+from collections import defaultdict
 class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
-        t={}
-        for i in range(1,len(parent)):
-            if parent[i] not in t:
-                t[parent[i]]=[i]
-            else:
-                t[parent[i]].append(i)
-            
+        
+        
+        graph = defaultdict(list)
+        for i,v in enumerate(parent):
+            graph[v].append(i)
+        
         answer = 1
         def dfs(vertex):
             nonlocal s
             nonlocal answer
-            if vertex not in t:
+            nonlocal graph
+            if vertex not in graph:
                 return 1
             res = 1
-            for child in t[vertex]:
+            for child in graph[vertex]:
                 ret = dfs(child)
                 if s[vertex] != s[child]:
                     answer = max(answer,res + ret)
