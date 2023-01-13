38bf8d9cfd9dcb0e8c9a19b09f5c56b0dae3ba1f
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-01-13 21:14:07 +0900

    Time: 1651 ms (92.31%), Space: 149 MB (75.30%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -18,7 +18,9 @@ class Solution:
             for child in graph[vertex]:
                 ret = dfs(child)
                 if s[vertex] != s[child]:
+                    # 
                     answer = max(answer,res + ret)
+                    # 자기자신 알파벳 더해서, 길이 비교해서 max 값 (이유는 제일 긴 길이 찾아주기위해서)( child 중1개만 선택해야하는상황)
                     res = max(res,ret + 1)
             return res
         dfs(0)
