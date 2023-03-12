58190c678fc3c0183c5e896fe4b8f3995fc66f3e
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-03-12 17:20:12 +0900

    Time: 469 ms (66.65%), Space: 47 MB (36.76%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -13,14 +13,19 @@ class Solution:
         def bfs(source,target):
             nonlocal bus
             q = deque([(source,0)])
+            # 버스를 방문체크한다
             visit = [0]*(501)
             while q:
                 station,cnt = q.popleft()
                 if station == target:
                     return cnt
+                # 현재 station 에 오는 버스들을 돌면서 체크 
                 for b in bus[station]:
+                    # 이전에 체크했던 버스면 굳이 체크 x
                     if visit[b] :continue
+                    #방문체크
                     visit[b] = 1
+                    # 이버스가 갈수있는 경우 다 체크 
                     for x in routes[b]:
                         q.append((x,cnt + 1))
             return -1
