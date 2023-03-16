3c0d319c8a081ae08cf2c48af20c29f4c39628ed
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-03-16 12:50:10 +0900

    Time: 912 ms (64.63%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -5,5 +5,14 @@ GROUP BY a.player_id
 HAVING "first_login" = MIN("first_login")
 
 
+#키포인트는 HAVING 절은 SELECT 절 이후를 필터링하는것이다 ->
+#WHERE 절느낌과 사뭇다름 둘다 필터링 역할을 하긴하지만 WHERE 는 select 이전에 필터링
+#HAVING 은 select 이후를 필터링
+#이전에 했던게 왜안되냐?
+#player_id 의 기록이 여러개있는데 아무집계함수없이 GROUP BY 를 하게되면 전혀엉뚱한결과가
+#나오게되고 -> 중복 다무시한채 아무결과가 나오게됨 -> 그말 즉 -> MIN 값을 제대로 찾지못하는현상
+
+
+
 
 
