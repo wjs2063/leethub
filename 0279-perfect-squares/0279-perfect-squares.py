ae5951beb9d46b9186eb8ef3d7e2cba75e613b16
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-03-30 19:32:43 +0900

    Time: 4849 ms (36.08%), Space: 13.9 MB (82.13%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,7 +1,8 @@
 from collections import deque
 class Solution:
     def numSquares(self, n: int) -> int:
-        # perfect square 
+        # perfect square  bfs solution
+        """
         seen = set()
         q = deque([(0,0)])
         square = []
@@ -19,4 +20,24 @@ class Solution:
                 seen.add((cnt + 1,root + sq))
                 q.append((cnt + 1,root + sq))
         
+        """
+        square = []
+        for i in range(1,n + 1):
+            if i**2 > n:
+                break
+            square.append(i**2)
+        """
+        dp solution
+        
+        """
+        # dp[i] := i를 square 합으로 만드는데 최소
+        INF = int(1e10)
+        dp = [INF]*(n + 1)
+        dp[0] = 0
+        for i in range(1,n + 1):
+            for s in square:
+                if i - s >= 0:
+                    dp[i] = min(dp[i],dp[i - s] + 1)
+        return dp[n]
+            
         
