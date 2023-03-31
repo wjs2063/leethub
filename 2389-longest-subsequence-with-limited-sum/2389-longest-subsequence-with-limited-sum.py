e54c0211ac2387f6a63aaa9ba7665eca6b1b98cc
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-03-31 16:14:44 +0900

    Time: 106 ms (80.82%), Space: 14.1 MB (71.19%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,17 +1,14 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        
-        nums = [0] + nums
+        from bisect import bisect_left,bisect_right
         n = len(nums)
         nums.sort()
         for i in range(1,n):
             nums[i] += nums[i - 1]
         ans = []
         for q in queries:
-            for i in range(n - 1,-1,-1):
-                if nums[i] > q:continue
-                ans.append(i)
-                break
+            idx = bisect_right(nums,q)
+            ans.append(idx)
         return ans
                     
         
