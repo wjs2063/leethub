5b5d00d487632d49262f55f22fef9660f8546a28
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-03-31 21:10:46 +0900

    Time: 555 ms (80.72%), Space: 28.1 MB (82.72%) - LeetHub

--- 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.py
+++ 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.py
@@ -2,16 +2,12 @@ from heapq import heappush,heappop,heapify
 class Solution:
     def maximizeGreatness(self, nums: List[int]) -> int:
         nums.sort()
-        sub = nums[:]
-        heapify(sub)
         ans = 0
-        for i,v in enumerate(nums):
-            while sub and v >= sub[0]:
-                heappop(sub)
-            if sub and sub[0] > v:
+        for v in nums:
+            if v > nums[ans]:
                 ans += 1
-                heappop(sub)
         return ans
+            
         
         
         
