5630207048b712a98c68028780bcabce3982eabb
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-04-02 16:58:37 +0900

    Time: 3010 ms (5.00%), Space: 53.2 MB (5.00%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        from collections import defaultdict
+        def find(a):
+            if parent[a] != a:
+                parent[a] = find(parent[a])
+            return parent[a]
+        def union(a,b):
+            x,y = find(a),find(b)
+            parent[x] = y 
+        
+        parent = [i for i in range(10**5 + 1)]
+        tree = defaultdict(list)
+        for p,c,is_left in descriptions:
+            tree[p].append([c,is_left])
+            parent[c] = p
+        
+        root = find(descriptions[0][0])
+        
+        def dfs(r):
+            root = TreeNode(r)
+            for c,is_left in tree[r]:
+                if is_left == 1:
+                    root.left = dfs(c)
+                else:
+                    root.right = dfs(c)
+            return root
+        return dfs(root)
+        
+        
+        
