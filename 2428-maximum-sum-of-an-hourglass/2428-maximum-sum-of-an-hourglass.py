419babcfe485c42f6aacbacb94471c638fe0d243
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-04-08 16:02:13 +0900

    Time: 494 ms (5.21%), Space: 17.2 MB (40.62%) - LeetHub

---
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        def in_range(x,y):
+            if 0 <= x < n and 0 <= y < m :return 1
+            return 0
+        
+        def solve(x,y):
+            s = 0
+            
+            for nx,ny in [(x - 1,y - 1),(x - 1,y),(x - 1,y + 1),(x,y),(x + 1,y - 1),(x + 1,y),(x + 1,y + 1)]:
+                if not in_range(nx,ny):return 0
+                s += grid[nx][ny]
+            return s 
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                ans = max(ans,solve(i,j))
+        return ans
+                
+            
+            
