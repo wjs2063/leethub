9648ae6c1a777e02c545d38b092756f3b2e267a9
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-04-14 14:10:05 +0900

    Time: 1496 ms (66.67%), Space: 14.4 MB (53.33%) - LeetHub

---
+++ 2059-minimum-operations-to-convert-number/2059-minimum-operations-to-convert-number.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minimumOperations(self, nums: List[int], start: int, goal: int) -> int:
+        from collections import deque
+        
+        def bfs(start,goal):
+            seen = set()
+            seen.add(start)
+            q = deque([(start,0)])
+            while q:
+                x,time = q.popleft()
+                print(x,time)
+                if x == goal:return time
+                
+                for i,v in enumerate(nums):
+                    if x + v == goal :return time + 1
+                    if x - v == goal :return time + 1
+                    if x ^ v == goal :return time + 1
+                    if 0 <= x + v <= 1000 and  x + v not in seen:
+                        
+                        seen.add(x + v)
+                        q.append((x + v,time + 1))
+                    if 0 <= x - v <= 1000 and x - v not in seen:
+                        
+                        seen.add(x - v)
+                        q.append((x - v,time + 1))
+                    if 0 <=  x ^ v <= 1000 and x ^ v not in seen:
+                        seen.add(x ^ v)
+                        q.append((x ^ v,time + 1))
+            return -1
+        return bfs(start,goal)
+            
