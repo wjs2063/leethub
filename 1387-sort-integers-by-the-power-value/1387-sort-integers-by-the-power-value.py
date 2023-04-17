8ccf2f0b762ea94559c3ea1aa59cbbec71d737a7
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-04-17 20:55:57 +0900

    Time: 189 ms (75.83%), Space: 29.2 MB (7.00%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,25 +1,20 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        from collections import deque
-        def bfs(x):
-            q = deque([(x,0)])
-            seen = set()
-            seen.add(x)
-            while q:
-                x,cnt = q.popleft()
-                if x == 1:return cnt
-                if x % 2 == 0 and x // 2 not in seen:
-                    q.append((x//2,cnt + 1))
-                    seen.add(x//2)
-                elif x % 2 and 3 * x + 1 not in seen:
-                    q.append((3 * x + 1,cnt + 1))
-                    seen.add(3 * x + 1)
-            return 0
+        from collections import defaultdict
+        dp = defaultdict(int)
+        
+        def power(x):
+            if x == 1: return 0
+            if x not in dp:
+                if x % 2 == 0 :
+                    dp[x] = power(x // 2) + 1
+                else:
+                    dp[x] = power(3 * x + 1) + 1
+            return dp[x]
         ans = []
-        for num in range(lo,hi + 1):
-            ans.append([num,bfs(num)])
+        for x in range(lo,hi + 1):
+            ans.append([x,power(x)])
         ans.sort(key = lambda x:(x[1],x[0]))
-        sub = []
-        for x,y in ans:
-            sub.append(x)
-        return sub[k - 1]
+        return ans[k - 1][0]
+                    
+                
