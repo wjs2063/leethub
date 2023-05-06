b1b4038cb4e05f49b8268293790dfbe188942753
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-05-06 20:54:59 +0900

    Time: 84 ms (18.33%), Space: 17 MB (8.13%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,26 @@
+from collections import deque
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        
+        def bfs(rooms):
+            vis = set()
+            q = deque([])
+            q.extend(rooms[0])
+            vis.add(0)
+            for x in rooms[0]:
+                vis.add(x)
+            while q:
+                x = q.popleft()
+                
+                for nx in rooms[x]:
+                    if nx not in vis:
+                        vis.add(nx)
+                        q.append(nx)
+            return vis
+        vis = bfs(rooms)
+        print(vis)
+        for i,v in enumerate(rooms):
+            if not v :continue
+            if i not in vis:return False
+        return True
+        
