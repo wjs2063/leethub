9786217f3ceb6d575434e521879601b49d66b931
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2024-02-28 21:25:09 +0900

    Time: 42 ms (56.61%), Space: 18.3 MB (79.63%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -4,18 +4,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        ans = []
-        q = deque([root])
-        while q:
-            x = q.popleft()
-            ans.append(x.val)
-            if x.right:
-                q.append(x.right)
-            if x.left:
-                q.append(x.left)
-        return ans[-1]
-
+        from collections import defaultdict
+        max_level = 0 
+        left_order_dict = defaultdict(list)
         
+        def dfs(root,left_order_dict,lv):
+            if root is None : return
+            nonlocal max_level
+            #LVR 
+            dfs(root.left,left_order_dict,lv + 1)
+            # ROOT
+            if lv > max_level:
+                max_level = lv
+            left_order_dict[lv].append(root.val)
+            dfs(root.right,left_order_dict,lv + 1)
+        dfs(root,left_order_dict,0)
+        return left_order_dict[max_level][0]
