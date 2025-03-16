d95333188dc5e12686cd5faad2beabc019cbbb8c
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2025-03-16 19:17:28 +0900

    Time: 3 ms (53.66%), Space: 19.9 MB (58.17%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
@@ -6,19 +6,19 @@
 #         self.right = right
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        INF = float("-inf")
-        lv = [INF]*(10**4 + 1)
-        def dfs(root,depth):
-            if not root : return
-            nonlocal lv
-            lv[depth] = max(root.val,lv[depth])
-            dfs(root.left,depth + 1)
-            dfs(root.right,depth + 1)
-        dfs(root,0)
+        from collections import deque 
+        level = defaultdict(list)
         ans = []
-        for i,v in enumerate(lv):
-            if v == INF:return ans 
-            ans.append(v)
+
+        def dfs(root:Optional[TreeNode],_level) -> None :
+            if root is None:return 
+            level[_level].append(root.val)
+            dfs(root.left,_level + 1)
+            dfs(root.right,_level + 1)
+        dfs(root,0)
+        for k,v in level.items():
+            ans.append(max(v))
         return ans
-        
+
+
         
